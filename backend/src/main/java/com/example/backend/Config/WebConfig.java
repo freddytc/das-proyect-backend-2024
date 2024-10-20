05dc290225415a4afa0081f7eb425cb93d482649
Rhista05 <u21307725@utp.edu.pe>
2024-10-19 23:26:04 -0500

    Adding WebConfig

---
+++ backend/src/main/java/com/example/backend/Config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") //  Applies to all routes
+                        .allowedOrigins("http://localhost:3000") //  URL of your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") //  Allowed methods
+                        .allowedHeaders("*") // Allowed headers
+                        .allowCredentials(true); // Allow sending cookies and credentials
+            }
+        };
+    }
+}
